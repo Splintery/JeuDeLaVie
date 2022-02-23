4de12c74d9ac41d3289e45453bd22491cb6aba7c
Thibault ROLLAND <thibault.rolland2@hotmail.fr>
2022-02-24 00:45:57 +0100

    Ajout des regles flexibles

--- test/Test.java
+++ test/Test.java
@@ -1,15 +1,10 @@
 package test;
-import model.Model;
-import model.Cellule;
-
 import controller.Controller;
 
 public class Test {
 
 	public static void main(String[] args) {
-			Controller c =new Controller();
-			Model a = new Model(c);
-			
+			new Controller();
 	}
 
 }
