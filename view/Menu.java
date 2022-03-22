85090a8f391048ffaf9ee90d2f0be1696ba1ccce
Aelxis N'gabala <angabala@outlook.com>
2022-03-22 10:40:54 +0100

    menu

--- view/Menu.java
+++ view/Menu.java
@@ -1,12 +1,15 @@
 package view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
 
 import controller.Controller;
 
@@ -16,9 +19,13 @@ public class Menu extends JPanel {
 	
 	private JButton playButton = new JButton("Lecture");
 	private JButton pauseButton = new JButton("Pause");
-	private JSlider zoomSlider = new JSlider(0, 100);
+	private JButton refreshButton = new JButton("Clear");
+	private JButton reglesButton = new JButton("Règles");
+	private JSlider vitesseSlider = new JSlider(0, 100);
 	public static int nbr=0;
+	private JLabel label = new JLabel();
 	private static JLabel nbrTour = new JLabel("Tour : "+nbr);
+
 	
 	public Controller controller;
 
@@ -36,12 +43,28 @@ public class Menu extends JPanel {
 	    pauseButton.setBounds(100, 170, 100, 30);
 	    add(pauseButton);
 	    
-	    zoomSlider.setBounds(100, 240, 100, 30);
-	    add(zoomSlider);
+	    vitesseSlider.setBounds(100, 240, 100, 30);
+	    add(vitesseSlider);
+	    
+	    label.setBounds(120, 280, 100, 30);
+	    add(label);
+	    
+	    reglesButton.setBounds(100, 400, 100, 30);
+	    add(reglesButton);
+	    
+	    label.setText("Vitesse : " + this.vitesseSlider.getValue() + "%");
+	    //this.vitesseSlider.addChangeListener(this);
+	    
+	    refreshButton.setBounds(100, 500, 100, 30);
+	    add(refreshButton);
 
 		nbrTour.setBounds(100, 650, 100, 50);
 		nbrTour.setForeground(Color.RED);
 		add(nbrTour);
+	}
+	
+	public void stateChanged(ChangeEvent e) {
+		label.setText("Vitesse : " + this.vitesseSlider.getValue() + "%");
 	}
 
 	public static void updateTour(){
@@ -50,15 +73,20 @@ public class Menu extends JPanel {
 		nbrTour.repaint();
 	}
 	
-
-
-	
 	public void addPlayButtonListener(ActionListener l) {
 		playButton.addActionListener(l);
 	}
 	
 	public void addPauseButtonListener(ActionListener l) {
 		pauseButton.addActionListener(l);
+	}
+	
+	public void addreglesButtonListener(ActionListener l) {
+		reglesButton.addActionListener(l);
+	}
+	
+	public void addRefreshButtonListener(ActionListener l) {
+		refreshButton.addActionListener(l);
 	}
 
 }
